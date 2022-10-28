38690bfeca789c147b40c8f79464a4f951283d20
prabalsh19 <prabal.sharma2003@gmail.com>
2022-10-28 12:43:34 +0530

    error handling
    error handling for negative and empty values.

--- main.js
+++ main.js
@@ -3,22 +3,35 @@ const numOfStocks = document.querySelector(".numOfStocks")
 const currentPrice = document.querySelector(".currentPrice")
 const submitBtn = document.querySelector(".submitBtn")
 const result = document.querySelector(".result")
-
+const err = document.querySelector(".err")
 
 const onSubmitHandler = ()=>{
     const initialValue = initialPrice.value*numOfStocks.value
     const difference =  currentPrice.value*numOfStocks.value - initialValue
     const percentage = difference/initialValue * 100
     console.log(difference);
-    if(difference > 0){
+    if(initialValue<0||numOfStocks<0||currentPrice.value<0){
+        err.innerText="Input values cannot be negative!"
+        err.style.color='red'
         
+    }else if(initialValue===''||numOfStocks.value===''||currentPrice.value===''){
+        err.innerText="Input values cannot be empty!"
+        err.style.color='red'
+
+    }
+    else if(difference > 0){
+        err.innerText=''
         result.innerText = "Yay!! Your Profit is "+difference+ " and profit Percentage is "+percentage+ "🥳"
         result.style.color = "white"
     }else if(difference < 0){
+        err.innerText=''
+
         result.innerText ="Whoops!! Your loss is "+difference+ " and loss Percentage is "+percentage+":("
         result.style.color = "red"
 
     }else{
+        err.innerText=''
+
         result.innerText = "No pain No gain, and no gain no pain :)"
         result.style.color = "white"
 
